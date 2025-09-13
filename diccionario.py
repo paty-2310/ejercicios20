6d44d6234b3fafc1cd64a3b08f3a3a0c96d78629
paty-2310 <patriciamayora86@gmail.com>
2025-09-13 12:09:53 -0400

    Add files via upload

---
+++ diccionario.py
@@ -0,0 +1,7 @@
+mi_diccionario = {
+    "nombre" : "patricia", 
+    "apellido" : "portillo", 
+    "color" : "morado pastel", 
+}
+for clave, valor in mi_diccionario.items(): 
+    print(f"\nclave: {clave}. valor: {valor}")
